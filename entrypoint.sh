76b9bfff1507ad5cae3dc74e25c2bb897a764048
ido <ido@nanya.business.com>
2021-03-08 11:12:15 +0200

    Use env varibale or .env based on condition

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 rm -rf web/static
 #. venv/bin/activate
-#Extract the databse and port number from the env file
-serverport=$(grep  DATABASE_URL ./secret/.env | cut -d '@' -f 2 | cut -d "/" -f 1)
+#Use the .env file if the key exists otherwise use an environment variable
+if grep -q DATABASE_URL ./secret/.env; then
+    url=$(grep  DATABASE_URL ./secret/.env)
+else
+    url=$DATABASE_URL
+fi
+#Extract the server and port number from the URL
+serverport=$(echo $url | cut -d '@' -f 2 | cut -d "/" -f 1)
 server=$(echo $serverport | cut -d ':' -f 1)
 port=$(echo $serverport | cut -d ':' -f 2)
 #Wait for the database to accept connection
@@ -10,4 +16,3 @@ while ! nc -z -v -w 3 $server $port; do sleep 5; done
 echo "Db is accepting connections"
 flask db init
 exec gunicorn -b :5000 --access-logfile - --error-logfile - chat:app
-grep  DATABASE_URL ./secret.env
